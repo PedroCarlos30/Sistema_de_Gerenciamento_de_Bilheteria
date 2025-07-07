c155977074be80780ad5f0e9224bb52766ea409a
Jackson Renan <147336900+Jacksonrs@users.noreply.github.com>
2025-07-06 22:43:58 -0300

    Create categoriaIngresso.java
    Implementação inicial da classe CategoriaIngresso

---
+++ categoriaIngresso.java
@@ -0,0 +1,38 @@
+package Sistema_de_Gerenciamento_de_Bilheteria;
+
+public class CategoriaIngresso {
+    private int idCategoria;
+    private String nomeCategoria;
+    private double precoBase;
+
+    public CategoriaIngresso(int idCategoria, String nomeCategoria, double precoBase) {
+        this.idCategoria = idCategoria;
+        this.nomeCategoria = nomeCategoria;
+        this.precoBase = precoBase;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaIngresso{" +
+                "idCategoria=" + idCategoria +
+                ", nomeCategoria='" + nomeCategoria + '\'' +
+                ", precoBase=" + precoBase +
+                '}';
+    }
+}
